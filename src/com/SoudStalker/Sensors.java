5d0e08545dd9e5534104b609bee7b011525ab0d9
RUBBA\Ruben Natvik <r_bn-95@hotmail.com>
2018-02-14 11:24:19 +0100

    test

--- src/com/SoudStalker/Sensors.java
+++ src/com/SoudStalker/Sensors.java
@@ -3,4 +3,18 @@ package com.SoudStalker;
 
 public class Sensors {
 
+    boolean ic2;
+    boolean serial;
+    boolean spi;
+    boolean uart;
+    int communicationIndex;
+
+    public Sensors(boolean ic2, boolean serial, boolean spi, boolean uart, int communicationIndex) {
+        this.ic2 = ic2;
+        this.serial = serial;
+        this.spi = spi;
+        this.uart = uart;
+        this.communicationIndex = communicationIndex;
+    }
+
 }
