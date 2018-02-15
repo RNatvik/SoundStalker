3197b47756a7d5935e80a9d7faa17b3db0960fe7
RUBBA\Ruben Natvik <r_bn-95@hotmail.com>
2018-02-15 17:41:59 +0100

    Removed useless code

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,9 @@ public class Main {
         Communications.setup();
         int i = 0;
         Thread.sleep(2000);
+
         while (true) {
-            long startTime = System.nanoTime();
             Communications.update();
-            long endTime = System.nanoTime();
-            System.out.println("Time to update: " + (endTime - startTime));
             Communications.printHashMap();
             System.out.println();
             i++;
