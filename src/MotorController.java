768db39cacdf91f3f6c3802cb2e931dd152f79e5
VesleRuben <itzRuben96@gmail.com>
2018-02-16 12:56:26 +0100

    lagt på noen komentarer i motorconntroller classen

--- src/MotorController.java
+++ src/MotorController.java
@@ -23,7 +23,9 @@ public class MotorController {
         MOTOR_L = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_23, "Left motor");
         MOTOR_R = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_24, "Right motor");
 
-        MOTOR_L.setPwm(1023);
-        MOTOR_R.setPwm(1023);
+        MOTOR_L.setPwm(1023);   // Sets the PWM frequency
+        MOTOR_R.setPwm(1023);   // Sets the PWM frequency
+        MOTOR_L.setPwmRange(0); // Sets the output signal to 0 V
+        MOTOR_R.setPwmRange(0); // Sets the output signal to 0 V
     }
 }
