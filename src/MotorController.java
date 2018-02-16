4e68b86b377666bd32e313b1c96053cb684b06d0
RUBBA\Ruben Natvik <r_bn-95@hotmail.com>
2018-02-16 14:23:02 +0100

    Changed code in MotorController

--- src/MotorController.java
+++ src/MotorController.java
@@ -1,3 +1,4 @@
+import com.Communications;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
@@ -5,8 +6,9 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public class MotorController {
 
-    private int leftSensorValue;
-    private int rightSensorValue;
+    private int leftSensorStrength;
+    private int rightSensorStrength;
+    private int sensorRelativeStrength;
     private int leftMotorPWM;
     private int rightMotorPWM;
 
@@ -17,15 +19,63 @@ public class MotorController {
 
     public MotorController() {
 
+        this.leftSensorStrength = 0;
+        this.leftMotorPWM = 0;
+        this.rightSensorStrength = 0;
+        this.rightMotorPWM = 0;
 
         this.GPIO = GpioFactory.getInstance();
 
-        MOTOR_L = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_23, "Left motor");
-        MOTOR_R = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_24, "Right motor");
+        this.MOTOR_L = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_23, "Left motor");
+        this.MOTOR_R = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_24, "Right motor");
 
-        MOTOR_L.setPwm(1023);   // Sets the PWM frequency
-        MOTOR_R.setPwm(1023);   // Sets the PWM frequency
-        MOTOR_L.setPwmRange(0); // Sets the output signal to 0 V
-        MOTOR_R.setPwmRange(0); // Sets the output signal to 0 V
+        this.MOTOR_L.setPwmRange(1023);  //Sets the PWM range
+        this.MOTOR_R.setPwmRange(1023);  //Sets the PWM range
+        this.MOTOR_L.setPwm(this.rightMotorPWM);          //Sets initial PWM output to 0
+        this.MOTOR_R.setPwm(this.rightMotorPWM);          //Sets initial PWM output to 0
+
+    }
+
+    public boolean doYourThing() {
+        boolean successful = false;
+        this.update();
+        int newRelativeStrength = getNewRelativeStrength();
+        if (newRelativeStrength - this.sensorRelativeStrength > 5 ||  newRelativeStrength - this.sensorRelativeStrength < -5) {
+            this.sensorRelativeStrength = newRelativeStrength;
+            calculateNewForwardPWM();
+            calculateNewTunrPWM();
+
+        } else {
+            System.out.println("Change in direction not large enough to change direction");
+            successful = true;
+        }
+        return successful;
+    }
+
+    private boolean calculateNewTunrPWM() {
+
+    }
+    /*
+    475 left
+    890 right
+    1023 / 500 = 2,04
+    1023 / 600 = 1,71
+     */
+
+    private boolean calculateNewForwardPWM() {
+        //if sensorRelativeStrength < 0, left motor must run faster than right.
+        boolean successful = false;
+        this.leftMotorPWM = (1024 / this.leftSensorStrength+1) -1;
+
+        return successful;
+    }
+
+    private int getNewRelativeStrength() {
+        return this.leftSensorStrength - this.rightSensorStrength;
+    }
+
+    private void update() {
+        this.leftSensorStrength = Communications.getSensorValue("irSensorL");
+        this.rightSensorStrength = Communications.getSensorValue("irSensorR");
     }
 }
