cf54bd130d1fc408ecd6a492f96eb28c1874637e
VesleRuben <itzRuben96@gmail.com>
2018-02-16 12:48:38 +0100

    Satt opp GPIO i motorcontroller classsen

--- src/MotorController.java
+++ src/MotorController.java
@@ -1,7 +1,29 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.RaspiPin;
+
 public class MotorController {
 
     private int leftSensorValue;
     private int rightSensorValue;
     private int leftMotorPWM;
     private int rightMotorPWM;
+
+    private final GpioController GPIO;
+
+    private final GpioPinPwmOutput MOTOR_L;
+    private final GpioPinPwmOutput MOTOR_R;
+
+    public MotorController() {
+
+
+        this.GPIO = GpioFactory.getInstance();
+
+        MOTOR_L = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_23, "Left motor");
+        MOTOR_R = this.GPIO.provisionPwmOutputPin(RaspiPin.GPIO_24, "Right motor");
+
+        MOTOR_L.setPwm(1023);
+        MOTOR_R.setPwm(1023);
+    }
 }
